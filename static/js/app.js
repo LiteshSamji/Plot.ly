f8e38345b9b3dfdafd138da5b9d82a4d51f8c455
Litesh Samji <litesh@y7mail.com>
2021-08-21 19:00:39 +1000

    Adding labels to the Bar and Bubble charts

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,24 @@
-function demoinfo(tableData) {
-    console.log(tableData)
-    var ul = d3.select("#sample-metadata");
-    //clear panel
-    ul.html("") 
-    Object.entries(tableData).forEach(([keys, value]) => {
-        ul.append("h5").text(`${keys} : ${value}`)
-        console.log(keys, value)
+//Step 1 - Use the D3 library to read in samples.json
+function init() {
+    var dropdown = d3.select("#selDataset") 
+    d3.json("samples.json").then(function (data) {
+        data.names.forEach((person) => {
+            dropdown.append("option")
+                .text(person)
+                .property("value", person)
+        })
+        //Use first ID as default
+        var initialdata = data.samples[0]
+        //console.log(data);
+        Barchart(initialdata)
+        Bubblechart(initialdata)
+        var metadata = data.metadata[0]
+        demoinfo(metadata)
     });
 }
 
-function optionChanged(selectednumber) {
-    d3.json("samples.json").then(function (data) {
-        var filtereddata = data.samples.filter(row => row.id == selectednumber)
-        console.log(filtereddata)
-        Barchart(filtereddata[0])
-        var barfilteredmeta = data.metadata.filter(row => row.id == selectednumber)
-        demoinfo(barfilteredmeta[0])
-    })
-}
-
+//Step 2 Create a horizontal bar chart with a dropdown menu to display 
+//the top 10 OTU's found in that individual
 function Barchart(tableData) {
     console.log(tableData.otu_ids.map(function (otu) {
         return "OTU " + otu
@@ -36,18 +36,18 @@ function Barchart(tableData) {
         y: slice.reverse(),
         orientation: 'h'
     }];
-
     var layout = {
-        title: 'Top 10 Bacteria Cultures Found',
-        barmode: 'stack',
-        xaxis: { title: "" },
-        yaxis: { title: "" },
-        width: 600
-    }
-        Plotly.newPlot("bar", trace1, layout);
+    title: 'Top 10 Bacteria Cultures Found',
+    xaxis: { title: "Bacteria Sample Values" },
+    yaxis: { title: "OTU IDs" }
+    };
+     // Display plot
+    Plotly.newPlot("bar", trace1, layout);
 }
 
-function Bubbledata(tableData) {
+//Step 3 - Create a bubble chart that display's each sample
+function Bubblechart(tableData) {
+    // Define samples
     var otu_label = tableData.otu_labels
     var number = tableData.sample_values
     var otu_id = tableData.otu_ids
@@ -59,37 +59,45 @@ function Bubbledata(tableData) {
         mode: 'markers',
         marker: {
             color: otu_id,
-            size: sample_values
+            size: sample_values,
+            colorscale: 'Earth'          
         }
     };
-
+    // Create the trace
     var data = [trace1];
-
     var layout = {
-        title: 'Bacteria',
-        xaxis: { title: "" },
-        yaxis: sample_values,
-        showlegend: false,
-        height: 600,
-        width: 1000
+        title: 'Belly button Samples',
+        xaxis: { title: 'OTU ID' },
+        yaxis: { title: 'sample_values'},     
     };
-
+     // Display plot
     Plotly.newPlot('bubble', data, layout);
 }
-function init() {
-    var dropdown = d3.select("#selDataset") //referencing back to html  and appending dropdown
-    d3.json("samples.json").then(function (data) {
-        data.names.forEach((person) => {
-            dropdown.append("option")
-                .text(person)
-                .property("value", person)
-        })
-        //line 17 starts looking at names list with ids. then looping through the people on list
-        var initialdata = data.samples[0]
-        console.log(data);
-        Barchart(initialdata)
-        var metadata = data.metadata[0]
-        demoinfo(metadata)
+
+//Step 4 Display the sample metadata i.e an individual's demographic information
+// Display each key-value pair from the metadata JSON object somewhere on the page.
+function demoinfo(tableData) {
+    console.log(tableData)
+    var ul = d3.select("#sample-metadata");
+    //clear panel
+    ul.html("") 
+    Object.entries(tableData).forEach(([keys, value]) => {
+        ul.append("h5").text(`${keys} : ${value}`)
+        console.log(keys, value)
     });
 }
-init()
+
+//Change the graphs when Patient Id is changed.
+function optionChanged(selectedID) {
+    d3.json("samples.json").then(function (data) {
+        var filtereddata = data.samples.filter(row => row.id == selectedID)
+        console.log(filtereddata)
+        Barchart(filtereddata[0])
+        Bubblechart(filtereddata[0])
+        var barfilteredmeta = data.metadata.filter(row => row.id == selectedID)
+        demoinfo(barfilteredmeta[0])
+    })
+}
+
+// call the init() function for default data
+init();
